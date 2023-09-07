8c805b9854e684a43fd90df85efc0bd8a59ddea4
MatteoAndriolo <matteo.andriolo.2@studenti.unipd.it>
2023-09-07 12:24:25 +0200

    working replace with using shortcut lead b

--- .config/nvim/lua/custom/snippets.lua
+++ .config/nvim/lua/custom/snippets.lua
@@ -1,5 +1,21 @@
+-- Create a table to store the cached formulas
+local formula_cache = {}
+
 local function insertFormula()
     local current_word = vim.fn.expand('<cword>') -- Get the word under the cursor
+
+    -- Check if the formula is already cached
+    if formula_cache[current_word] then
+        local current_line = vim.fn.line('.') -- Get the current line number
+        local current_col = vim.fn.col('.') -- Get the current column number
+
+        -- Replace the word under the cursor with the cached formula
+        local formula = formula_cache[current_word]
+        vim.fn.setline(current_line, string.gsub(vim.fn.getline(current_line), current_word, formula))
+
+        return
+    end
+
     local current_dir = vim.fn.expand('%:p:h') -- Get the current directory of the file being edited
     local csv_path = current_dir .. '/shortcut.csv'
     local file = io.open(csv_path, 'r')
@@ -7,14 +23,18 @@ local function insertFormula()
         for line in file:lines() do
             local k, formula = line:match('([^,]+),([^,]+)')
             if k and k == current_word then
-                vim.fn.setreg('"', formula) -- Set the unnamed register with the formula
-                vim.cmd('normal! ""p') -- Paste the unnamed register at the cursor position
+                formula_cache[current_word] = formula -- Cache the formula
+
+                local current_line = vim.fn.line('.') -- Get the current line number
+                local current_col = vim.fn.col('.') -- Get the current column number
+
+                -- Replace the word under the cursor with the new formula
+                vim.fn.setline(".", formula)
+
                 file:close()
                 return
             end
         end
-        -- print error message if no formula was found
-        print('No formula found for keyword: ' .. current_word)
         file:close()
     end
 end
